712a050990e61754821071e184db6f5270eeaa33
yangzho12138 <YangZhou12138@outlook.com>
2022-08-02 14:00:54 +0800

    modify allowed host

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8xz4ewtx2v+e%8klnsciyb(5fm59rb-akqis=&z*y!_hohdpk&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["121.5.68.237"]
 
 
 # Application definition
